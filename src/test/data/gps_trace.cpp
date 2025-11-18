a5d799bd4449ad0532489563d15a8fbe820faea3
Lorenzo Framarin <lore.framarin@gmail.com>
2025-11-18 10:32:41 +0100

    Added gps_trace tests
    Improved parsing test
    Added Navigation test to check the next_point method behavior.
    Added tests for invalid GPX files and valid but empty GPX files.

--- src/test/data/gps_trace.cpp
+++ src/test/data/gps_trace.cpp
@@ -36,7 +36,9 @@ TEST(GpsTraceTest, Parsing) {
   </trk>
 </gpx>)");
 
-    static gps_trace trace(gpx_data, 47.86675, 15.16357, 10);
+    times_t ref_time = 10;
+
+    static gps_trace trace(gpx_data, 47.86675, 15.16357, ref_time);
 
     EXPECT_EQ(trace.size(), 2);
 
@@ -47,13 +49,66 @@ TEST(GpsTraceTest, Parsing) {
     ASSERT_FALSE(td_b.track.empty());
     EXPECT_EQ(td_a.track.size(), 3);
     EXPECT_EQ(td_b.track.size(), 2);
-    EXPECT_EQ(td_a.index, 0);
-    EXPECT_EQ(td_b.index, 0);
 
+    //check track points values to verify correct conversions
     EXPECT_EQ(td_a.track[0].x, 0);
     EXPECT_EQ(td_a.track[0].y, 0);
-    
-    EXPECT_GT(td_a.track[1].y, 0.0);
+    EXPECT_EQ(td_a.track[0].timestamp, ref_time);
     EXPECT_LT(td_a.track[1].x, 0.0);
+    EXPECT_GT(td_a.track[1].y, 0.0);
     EXPECT_GT(td_a.track[1].timestamp, td_a.track[0].timestamp);
+}
+
+TEST(GpsTraceTest, Navigation) {
+    std::string gpx_data = std::string(R"(<?xml version="1.0"?>
+<gpx>
+  <trk>
+    <trkseg>
+      <trkpt lat="47.86675" lon="15.16357">
+        <ele>833.15</ele>
+        <time>2018-05-21T04:51:34Z</time>
+      </trkpt>
+      <trkpt lat="47.86712" lon="15.16338">
+        <ele>833.27</ele>
+        <time>2018-05-21T04:52:14Z</time>
+      </trkpt>
+      <trkpt lat="47.86772" lon="15.16324">
+        <ele>834.12</ele>
+        <time>2018-05-21T04:52:56Z</time>
+      </trkpt>
+    </trkseg>
+  </trk>
+</gpx>)");
+
+    static gps_trace trace(gpx_data, 47.86675, 15.16357, 10);
+
+    EXPECT_EQ(trace.size(), 1);
+
+    track_data& td = trace.find_track(0);
+    ASSERT_FALSE(td.track.empty());
+
+    track_point& pt1 = trace.next_point(td, 0.0);
+    EXPECT_EQ(td.index, 0);
+    track_point& pt2 = trace.next_point(td, 30.0);
+    EXPECT_EQ(td.index, 1);
+    // When going past the last trackpoint the index becomes equal to track.size() to be reset in follow_track()
+    track_point& pt3 = trace.next_point(td, 100.0);
+    EXPECT_EQ(td.index, 3);
+}
+
+TEST(GpsTraceTest, InvalidGpx) {
+    std::string invalid_gpx = "Lorem Ipsum";
+    
+    EXPECT_THROW({
+        gps_trace trace(invalid_gpx, 0.0, 0.0, 0.0);
+    }, std::runtime_error);
+}
+
+TEST(GpsTraceTest, EmptyGpx) {
+    std::string empty_gpx = R"(<?xml version="1.0"?>
+<gpx>
+</gpx>)";
+    
+    gps_trace trace(empty_gpx, 0.0, 0.0, 0.0);
+    EXPECT_EQ(trace.size(), 0);
 }

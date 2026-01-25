0f3ad00fd80cb1b1805e9d89d98846171b91b29e
Giorgio Audrito <giorgio.audrito@gmail.com>
2026-01-25 12:28:36 +0100

    allowing degenerate half_radius values in connect::radial

--- src/lib/option/connect.hpp
+++ src/lib/option/connect.hpp
@@ -202,7 +202,7 @@ class powered : public fixed<num,den,n> {
  */
 template <intmax_t r50, typename C>
 class radial : public C {
-    static_assert(1 <= r50 and r50 <= 99, ANSI_START "half radius out of bounds" ANSI_END);
+    static_assert(0 <= r50 and r50 <= 100, ANSI_START "half radius out of bounds" ANSI_END);
 
     //! @brief Shortcut to the half_radius tag.
     using half_radius = component::tags::half_radius;
